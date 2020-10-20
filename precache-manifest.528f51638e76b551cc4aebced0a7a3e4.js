5e0fe2aca55399d0ae3639027dd3adc5b23f1726
JKYROC <JKYROC@gmail.com>
2020-10-21 04:05:24 +0800

    Updates

---
+++ precache-manifest.528f51638e76b551cc4aebced0a7a3e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b1352a1721766402f8261f5f1b63f2",
+    "url": "/JKYROC/RobotFriendsDemo.git/index.html"
+  },
+  {
+    "revision": "a31973fbfc2024124cf9",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "256e4e6770f3c7da9153",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/css/main.bd2e7c37.chunk.css"
+  },
+  {
+    "revision": "a31973fbfc2024124cf9",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/js/2.e6d0bb17.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/js/2.e6d0bb17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256e4e6770f3c7da9153",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/js/main.a0bdb56c.chunk.js"
+  },
+  {
+    "revision": "b5e4a30b3f92828e5068",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/js/runtime-main.e4c1aaa1.js"
+  },
+  {
+    "revision": "d3bebba97d549436fd6137d05d55ae33",
+    "url": "/JKYROC/RobotFriendsDemo.git/static/media/DancingScript.d3bebba9.ttf"
+  }
+]);
